c51f16dc9fac1fb20f3c2f6eec2589d2e0b91af8
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2023-07-01 22:14:38 +0530

    Update ans8.py

--- assignmnetPPT/day5/ans8.py
+++ assignmnetPPT/day5/ans8.py
@@ -1,35 +1,33 @@
 from collections import Counter
 
 def findOriginalArray(changed):
-    if len(changed) % 2 != 0:
-        return []
-    
-    original = []
-    hashmap = {}
-
-    # Create a hashmap with the frequency of elements
-    hashmap = Counter(changed)
-    
-    changed.sort()
-    n = len(changed)//2
-
-    for i in range(n):
-        if changed[i] * 2 in hashmap:
-
-            hashmap[changed[i] * 2] -=1
-            hashmap[changed[i]] -=1
-
-            original.append(changed[i])
-
-            if hashmap[changed[i] * 2] == 0:
-                hashmap.pop(changed[i] * 2)
-
-            if hashmap[changed[i]] == 0:
-                hashmap.pop(changed[i])
-        # else : 
-            # return []
-    
-    return original
+    counter = Counter(changed)
+    res = []
+    for k in counter.keys():
+        
+        if k == 0:
+            # handle zero as special case
+            if counter[k] % 2 > 0:
+                return []
+            res += [0] * (counter[k] // 2)
+            
+        elif counter[k] > 0:
+            x = k
+            
+            # walk down the chain
+            while x % 2 == 0 and x // 2 in counter:
+                x = x // 2
+                
+            # walk up and process all numbers within the chain. mark the counts as 0
+            while x in counter:
+                if counter[x] > 0:
+                    res += [x] * counter[x]
+                    if counter[x+x] < counter[x]:
+                        return []
+                    counter[x+x] -= counter[x]
+                    counter[x] = 0
+                x += x
+    return res
 
 changed = [1, 3, 4, 2, 6, 8]
 original = findOriginalArray(changed)
