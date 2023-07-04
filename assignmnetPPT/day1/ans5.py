38c9ae04aaf78580735d21852f16821207f5ac51
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2023-07-04 20:07:16 +0530

    Update ans5.py

--- assignmnetPPT/day1/ans5.py
+++ assignmnetPPT/day1/ans5.py
@@ -1,46 +1,32 @@
-def merge_sorted_arrays(nums1, m, nums2, n):
-  """
-  Merges the two sorted arrays nums1 and nums2 into a single sorted array.
+def merge(nums1, m, nums2, n):
+    # Initialize pointers for nums1, nums2, and the merged array
+    p1 = m - 1  # Pointer for nums1
+    p2 = n - 1  # Pointer for nums2
+    p = m + n - 1  # Pointer for the merged array
 
-  Args:
-    nums1: The first sorted array.
-    m: The number of elements in nums1.
-    nums2: The second sorted array.
-    n: The number of elements in nums2.
+    # Merge nums1 and nums2 from the end
+    while p1 >= 0 and p2 >= 0:
+        if nums1[p1] >= nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
 
-  Returns:
-    The merged sorted array.
-  """
+    # Copy remaining elements from nums2 to nums1
+    while p2 >= 0:
+        nums1[p] = nums2[p2]
+        p2 -= 1
+        p -= 1
 
-  i = 0
-  j = 0
-  k = 0
-  while i < m and j < n:
-    if nums1[i] <= nums2[j]:
-      nums1[k] = nums1[i]
-      i += 1
-    else:
-      nums1[k] = nums2[j]
-      j += 1
-    k += 1
-  while i < m:
-    nums1[k] = nums1[i]
-    i += 1
-    k += 1
-  while j < n:
-    nums1[k] = nums2[j]
-    j += 1
-    k += 1
+    # nums1 is already updated with the merged array
 
+# Test the function
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
 
-def main():
-  nums1 = [1, 2, 3, 0, 0, 0]
-  m = 3
-  nums2 = [2, 5, 6]
-  n = 3
-  merge_sorted_arrays(nums1, m, nums2, n)
-  print(nums1)
-
-
-if __name__ == "__main__":
-  main()
+merge(nums1, m, nums2, n)
+print(nums1)  # Output: [1, 2, 2, 3, 5, 6]
